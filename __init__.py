27f59d3f4b1b68aadf0fed43af4b5a3e483d59c6
Christian Fobel <christian@fobel.net>
2019-08-01 11:56:44 -0400

    fix(all): re-render from plugin template
    Re-render contents based on `gh:microdrop-plugin-cookiecutter` template.

--- __init__.py
+++ __init__.py
@@ -15,8 +15,6 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-logger = logging.getLogger(__name__)
-
 PluginGlobals.push_env('microdrop.managed')
 
 
